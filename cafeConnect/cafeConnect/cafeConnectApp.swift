0a35bf1f7d836c373c9a4c0b8976b8427ae04e13
Ellie Kim <elliekim100@gmail.com>
2024-02-24 14:02:49 -0500

    Added an XCode project

---
+++ cafeConnect/cafeConnect/cafeConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafeConnectApp.swift
+//  cafeConnect
+//
+//  Created by Ellie Kim on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct cafeConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
